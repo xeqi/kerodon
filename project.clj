2c3981c3b3aa3694355c6c7eee48017332333cdc
Nelson Morris <nmorris@nelsonmorris.net>
2012-03-05 22:46:34 -0600

    Release 0.0.1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject kerodon "0.0.1-SNAPSHOT"
+(defproject kerodon "0.0.1"
   :description "Acceptance test framework for web applications"
   :url "https://github.com/xeqi/kerodon"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [peridot "0.0.2-SNAPSHOT"]
+                 [peridot "0.0.3"]
                  [enlive "1.0.0"]]
   :profiles {:test {:dependencies [[net.cgrand/moustache "1.1.0"]
                                    [hiccup "1.0.0-beta1"]]}
