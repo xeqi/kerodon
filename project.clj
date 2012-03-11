d44a1b0b9fb51735c7e2b36f7637a3fe794901ee
Nelson Morris <nmorris@nelsonmorris.net>
2012-03-10 20:31:34 -0600

    Exclude enlive/moustache depedencies with version ranges

--- project.clj
+++ project.clj
@@ -4,8 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [peridot "0.0.3"]
-                 [enlive "1.0.0"]]
-  :profiles {:test {:dependencies [[net.cgrand/moustache "1.1.0"]
+                 [enlive "1.0.0" :exclusions [org.clojure/clojure]]]
+  :profiles {:test {:dependencies [[net.cgrand/moustache "1.1.0"
+                                    :exclusions
+                                    [[org.clojure/clojure]
+                                     [ring/ring-core]]]
+                                   [ring/ring-core "1.0.2"]
                                    [hiccup "1.0.0-beta1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta1"]]}}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0-beta4"]]}}
   :aliases {"all" ["with-profile" "test:test,1.4"]})
