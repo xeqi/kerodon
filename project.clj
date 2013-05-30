7a1bf50371bffbb44a056a211e792bd88e58b29c
Nelson Morris <nmorris@nelsonmorris.net>
2013-05-29 21:29:10 -0500

    Update dependencies and version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject kerodon "0.1.1-SNAPSHOT"
+(defproject kerodon "0.2.0-SNAPSHOT"
   :description "Acceptance test framework for web applications"
   :url "https://github.com/xeqi/kerodon"
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [peridot "0.1.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [peridot "0.2.1"]
                  [enlive "1.0.0" :exclusions [org.clojure/clojure]]
                  [ring/ring-codec "1.0.0"]
                  [org.flatland/ordered "1.5.1"]]
@@ -16,6 +16,5 @@
                                    [ring/ring-core "1.1.5"]
                                    [hiccup "1.0.1"]]
                     :resource-paths ["test-resources"]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
-  :aliases {"all" ["with-profile" "test:test,1.4:test,1.5"]})
+  :aliases {"all" ["with-profile" "test:test,1.5"]})
