33c24c6f1c88d6588418a752fffc48ceaca5550e
Nelson Morris <nmorris@nelsonmorris.net>
2014-01-25 19:48:44 -0600

    Bump version for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kerodon "0.3.0"
+(defproject kerodon "0.3.1-SNAPSHOT"
   :description "Acceptance test framework for web applications"
   :url "https://github.com/xeqi/kerodon"
   :min-lein-version "2.0.0"
