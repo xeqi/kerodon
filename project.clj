9021f762a5030a0c7ffb4f7f49b18e03393b882a
Christopher Mark Gore <cgore@cgore.com>
2015-08-14 14:40:56 -0500

    remove :dev profile now that 1.7 is default

--- project.clj
+++ project.clj
@@ -16,8 +16,6 @@
                                    [javax.servlet/servlet-api "2.5"]
                                    [hiccup "1.0.5"]]
                     :resource-paths ["test-resources"]}
-             :dev  {:dependencies [^:replace [org.clojure/clojure "1.6.0"]
-                                   [ring/ring-core "1.4.0"]]}
              :1.4  {:dependencies [^:replace [org.clojure/clojure "1.4.0"]
                                    ^:replace [ring/ring-core "1.3.2"]]}
              :1.5  {:dependencies [^:replace [org.clojure/clojure "1.5.1"]
