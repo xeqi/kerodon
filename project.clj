df614520cfa607e71d2ba7cd9ccd02496e6e597e
Glen Mailer <glenjamin@gmail.com>
2016-06-12 21:21:33 +0100

    Release 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kerodon "0.7.0"
+(defproject kerodon "0.8.0"
   :description "Acceptance test framework for web applications"
   :url "https://github.com/xeqi/kerodon"
   :min-lein-version "2.0.0"
